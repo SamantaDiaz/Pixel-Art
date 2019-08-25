9915d3afb534d2e7cfe9ce5778baedfd42ec92d3
Samanta <samantadvdiaz@gmail.com>
2019-08-24 22:37:19 -0300

    Paleta de color, pintar(), Rueda de color y pintarEnMovimiento() terminados

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -24,13 +24,14 @@ var nombreColores = ['White', 'LightYellow',
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
 
+var indicadorColor = document.getElementById("indicador-de-color");
+var colorActual;
+
 colorPersonalizado.addEventListener('change', 
   (function() {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
-    // Completar para que cambie el indicador-de-color al colorActual
-
-
+    indicadorColor.style.backgroundColor = colorActual;
   })
 );
 
@@ -38,26 +39,53 @@ var paleta = document.getElementById("paleta");
 var grillaPixeles = document.getElementById("grilla-pixeles");
 
 //Función para crear la paleta de colores dinámicamente
-
 function crearPaleta() {
   for (var i = 0; i < nombreColores.length; i++) {
       var color = document.createElement("div");
       paleta.appendChild(color);
       color.style.backgroundColor = nombreColores[i];
       color.className = "color-paleta";
-      // color.addEventListener("click", guardarColor)
+      color.addEventListener("click", guardarColor);
   }
 };
 
 // Función para crear la grilla con los píxeles dinámicamente
-
 function crearPixeles() {
     for (var i = 0; i < 1750; i++) {
         var pixel = document.createElement("div");
         grillaPixeles.appendChild(pixel);
+        pixel.addEventListener("click", pintar);
+        pixel.addEventListener("mousedown", presionado);
+        pixel.addEventListener("mouseup", presionado);
+        pixel.addEventListener("mouseover", pintarEnMovimiento);
     };
      
 };
+
+//Función que guarda el color en el indicador de color
+function guardarColor(e) {
+  colorActual = e.target.style.backgroundColor;
+  indicadorColor.style.backgroundColor = colorActual;
+};
+
+//Función que permite pintar los píxeles
+function pintar(e) {
+  e.target.style.backgroundColor = colorActual;
+};
+
+var mApretado = false;
+
+function presionado(e) {
+  mApretado = !mApretado;
+  console.log(mApretado);
+};
+
+function pintarEnMovimiento(e) {
+  if(mApretado) {
+    e.target.style.backgroundColor = colorActual;
+  };
+}
+
 
 crearPaleta();
 crearPixeles();
